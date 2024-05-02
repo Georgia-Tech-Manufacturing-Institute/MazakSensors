5c9637a72c75479ff27511b3db97d10effc2533d
nvanhand <73389036+nvanhand@users.noreply.github.com>
2024-05-02 16:08:58 -0400

    update for single line read

--- temp.py
+++ temp.py
@@ -1,31 +1,65 @@
 import numpy as np
 import board
-import analogio
-import adafruit_tca9548a
 import time
+import daqhats
+from daqhats import mcc118, OptionFlags
+from sys import stdout
 
 # pip install -r requirements.txt
 # https://github.com/adafruit/circup/issues/143 - Resolve with pip install setuptools
 #
 
-def temp_monitoring(sample_rate=0.1):
+def monitoring(sample_rate=0.1, timeout=10, channel_ranges=[(0,7)]):
     '''
-    Setup: RPi 4, adafruit AD8495 thermocouple amplifiers, type K thermocouples
-    Amplifier outputs are connected to a multiplexor -> Mux to
-    AD8495 https://learn.adafruit.com/ad8495-thermocouple-amplifier/python-circuitpython
-    Multiplexor https://learn.adafruit.com/adafruit-tca9548a-1-to-8-i2c-multiplexer-breakout/circuitpython-python
+
+    :param sample_rate:
+    :param timeout:
+    :param channels: list of tuples; for each hat, specify the range of channels to cread
     :return:
     '''
     def convert_temp(pin): # Conversion from voltage to
         voltage = (pin.value * 3.3) / 65536
         return (voltage - 1.25) / 0.005
 
+    # Initialization
+    start_time = time.time()
+    hat_list = daqhats.hat_list()
 
-    i2c = board.I2C()  # uses board.SCL and board.SDA
-    tca = adafruit_tca9548a.TCA9548A(i2c) # Create TCA9548A object and give it the I2C bus
-    temp_array = convert_temp(np.array(tca)) # should do it to the whole array but we'll see
+    if len(hat_list) > 1:
+        print('Identified Boards: ', len(hat_list))
+
+        devices = []
+        for i, hat in enumerate(hat_list): # Allows for multiple boards
+            address = hat.address # int
+            try:
+                print(channel_ranges[i])
+            except IndexError:
+                lc, hc = 0, 7
+            devices.append(mcc118(address=address))
+            print('Blinking HAT: ', address)
+            devices[-1].blink_led(count=10) # Blink to show that you exist !
+    else:
+        print('No Boards found')
+        return None
+
+    options = OptionFlags.DEFAULT
+    condition = True
+    data_array = np.zeros(len())
+    while condition:
+        temp_array = convert_temp(np.array(tca)) # should do it to the whole array but we'll see
+
+        for hat in devices:
+            for chan in range(low_chan, high_chan + 1):
+                value = hat.a_in_read(chan, options)
+
+        if timeout == False:
+            condition = True
+        else: # Check if timeout exceeded
+            condition = (time.time() - start_time) > timeout
+
+        time.sleep(sample_rate)
 
     return temp_array
 
 if __name__ == '__main__':
-    temp_monitoring(1)
+    monitoring(1)
